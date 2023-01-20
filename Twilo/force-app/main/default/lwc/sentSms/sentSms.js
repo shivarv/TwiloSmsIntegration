4bab94e5504f273d34711763dd7209200656f295
shiva <shivarv1993@gmail.com>
2023-01-20 17:51:02 +0000

    code changes for lwc and removed std fields

--- Twilo/force-app/main/default/lwc/sentSms/sentSms.js
+++ Twilo/force-app/main/default/lwc/sentSms/sentSms.js
@@ -4,12 +4,19 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { CloseActionScreenEvent } from 'lightning/actions';
 
 export default class SentSms extends LightningElement {
+    constructor() {
+        super();
+    }
+
     @api recordId;
     sentTwiloSms(event) {
         console.log('in sentTwiloSms');
-        //var inp = this.template.querySelector("lightning-input");
-
+        var inp = this.template.querySelector("lightning-textarea");
+//tteest
         
+        if(!input || !input.value ||  (input.value.length === 0 )) {
+            alert('Empty string, please enter the message before typing sent');
+        }
         sentSmsToCustomerMethod({smsBody: inp.value, accountId: this.recordId}).then(result => {
             console.log('in sentSmsToCustomerMethod success');
 
